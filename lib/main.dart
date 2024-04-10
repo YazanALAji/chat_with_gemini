d59de465983a45329dfa36439a7dd46e7bea5f1d
YazanALAji <yazan.alaji94@gmail.com>
2024-04-11 00:26:50 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chat_with_gemini/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  Gemini.init(
+    apiKey: "${dotenv.env["GEMINI_API_KEY"]}",
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
